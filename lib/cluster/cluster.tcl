350cf337db6a10e9139a1a81d2edc99bcf62962f
Emmanuel Frécon <efrecon@gmail.com>
2017-12-18 12:03:44 +0100

    Relative path for docker machine (WIP)

--- lib/cluster/cluster.tcl
+++ lib/cluster/cluster.tcl
@@ -1968,7 +1968,35 @@ proc ::cluster::candidates { {dir .} } {
 #
 ####################################################################
 
+# get relative path to target file from current file (end of http://wiki.tcl.tk/15925)
+proc ::cluster::RelativeTo {targetfile {currentpath ""}} {
+    if { $currentPath eq "" } {
+        set currentPath [pwd]
+    }
 
+    set cc [file split [file normalize $currentpath]]
+    set tt [file split [file normalize $targetfile]]
+    if {![string equal [lindex $cc 0] [lindex $tt 0]]} {
+        # not on *n*x then
+        return -code error "$targetfile not on same volume as $currentpath"
+    }
+    while {[string equal [lindex $cc 0] [lindex $tt 0]] && [llength $cc] > 0} {
+        # discard matching components from the front
+        set cc [lreplace $cc 0 0]
+        set tt [lreplace $tt 0 0]
+    }
+    set prefix ""
+    if {[llength $cc] == 0} {
+        # just the file name, so targetfile is lower down (or in same place)
+        set prefix "."
+    }
+    # step up the tree
+    for {set i 0} {$i < [llength $cc]} {incr i} {
+        append prefix " .."
+    }
+    # stick it all together (the eval is to flatten the targetfile list)
+    return [eval file join $prefix $tt]
+}
 
 # ::cluster::StorageDir -- Path to machine storage cache
 #
@@ -1994,6 +2022,7 @@ proc ::cluster::StorageDir { yaml } {
         file mkdir $dir;   # Let it fail since we can't continue otherwise
     }
     
+    return [RelativeTo $dir [pwd]]
     return [file normalize $dir]
 }
 
